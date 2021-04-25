5dbeccdbc01b3cc6a69fc3e9e1d1dfe6997c5cf8
Eric <lee.erictae@gmail.com>
2021-04-24 20:26:01 -0500

    add random method

--- src/views/MusclesRandom.vue
+++ src/views/MusclesRandom.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="muscles-random">
     <h1>{{ message }}</h1>
+    <button v-on:click="getRandomMuscleFrontend">Get Random Muscle (frontend) </button>
+    <p>{{ randomMuscle }}</p>
   </div>
 </template>
 
@@ -8,13 +10,26 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to the random page",
+      randomMuscle: {},
     };
   },
   created: function () {},
-  methods: {},
+  methods: {
+    getRandomMuscleFrontend: function () {
+      console.log("getting muscle...");
+      axios.get("/api/muscles").then((response) => {
+        console.log(response.data);
+        this.randomMuscle =
+          response.data[Math.floor(Math.random() * response.data.length)];
+        console.log(this.randomMuscle);
+      });
+    },
+  },
 };
 </script>
