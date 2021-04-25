a848d2ab6f8ad8960d7cd9a192a988e9b375c2d8
Eric <lee.erictae@gmail.com>
2021-04-25 00:32:55 -0500

    add a button show route and button (doesn't work yet)

--- src/views/Muscles.vue
+++ src/views/Muscles.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="home">
+  <div class="muscles">
     <h1>{{ message }}</h1>
+    
+    <button v-on:click="exercisesShow">Show Exercises </button>
     <div v-for="muscle in muscles">
       {{ muscle.name }}
+      <img v-bind:src="muscle.image_url">
       <hr />
     </div>
     
@@ -10,6 +13,9 @@
 </template>
 
 <style>
+img {
+  width: 250px;
+}
 </style>
 
 <script>
@@ -32,6 +38,9 @@ export default {
         console.log(response.data);
         this.muscles = response.data;
       });
+    },
+    exercisesShow: function () {
+      console.log("in exercises show");
     },
   },
 };
