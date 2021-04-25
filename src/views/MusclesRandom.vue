f011a77a10dd3c73f32393ab4c03fedbd91bdb20
Eric <lee.erictae@gmail.com>
2021-04-24 20:14:12 -0500

    add random page

---
+++ src/views/MusclesRandom.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="muscles-random">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the random page",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
